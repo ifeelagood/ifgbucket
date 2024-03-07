c515fb18505c3a4514b27571584ab7d53549b243
ifeelagood <103016468+ifeelagood@users.noreply.github.com>
2024-03-07 19:50:03 +1100

    Create quickbms-proxy.ps1

---
+++ scripts/quickbms-proxy.ps1
@@ -0,0 +1,22 @@
+param(
+    [String] $pfx,
+    [String] $exe
+)
+
+echo @"
+`$pfx=$pfx
+`$exe=$exe
+
+# copy arguments
+`$_args = `$args
+
+# add prefix to script argument
+if (`$_args.Length -gt 0) { `$_args[0] = `$pfx + `$_args[0] } else { echo 'Usage: quickbms.ps1 <script>'; exit -1 }
+
+# test script exists
+if (! `$(Test-Path `$_args[0])) { echo "script `$(`$_args[0]) not found"; exit -1; }
+
+# forward all arguments
+& `$exe @_args
+
+"@
